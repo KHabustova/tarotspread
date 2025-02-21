90288e70c978b057045b106421540c40e3575fbf
KHabustova <habustovakr@gmail.com>
2025-02-21 21:06:54 +0100

    Initial components card and board created, Card still not letting itself be sized properly

---
+++ app/Components/Board.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "./Card";
+
+function Board() {
+    return (
+        <div className="grid grid-cols-5 gap-4 grid-rows-auto  h-full">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default Board;
---
+++ app/Components/Card.jsx
@@ -0,0 +1,9 @@
+function Card () {
+    return (
+        <div className="bg-cyan-500 shadow-xl cursor-pointer w-full h-full"git >
+
+        </div>
+    )
+}
+
+export default Card;
